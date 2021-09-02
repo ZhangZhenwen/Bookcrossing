1defa7cf72a1a7c4ec9551b4e920867e35e38a7b
zhenwen <13075808821@163.com>
2021-09-02 17:54:17 +0800

    'init'

---
+++ bookcrossing/src/test/java/priv/zhenwen/bookcrossing/BookcrossingApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.zhenwen.bookcrossing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcrossingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
